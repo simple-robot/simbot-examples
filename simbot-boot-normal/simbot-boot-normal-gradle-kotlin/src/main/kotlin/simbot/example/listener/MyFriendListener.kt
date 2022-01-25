1a169cac6f9621c8bb51ada149b5da7561c7ea6e
ForteScarlet <ForteScarlet@163.com>
2022-01-25 22:45:17 +0800

    kotlin 示例

--- simbot-boot-normal/simbot-boot-normal-gradle-kotlin/src/main/kotlin/simbot/example/listener/MyFriendListener.kt
+++ simbot-boot-normal/simbot-boot-normal-gradle-kotlin/src/main/kotlin/simbot/example/listener/MyFriendListener.kt
@@ -21,6 +21,23 @@ suspend fun FriendMessageEvent.myListen() {
 }
 
 
+/**
+ * 接收一个好友消息事件 [FriendMessageEvent], 并实现如下会话场景:
+ *
+ * ```
+ * > 好友：绑定                 (1)
+ * > bot：请输入账号            (2)
+ * > 好友：xxxx                (3)
+ * > bot：绑定成功！账号：xxxx   (4)
+ *```
+ *
+ * 你在测试的时候会发现，在进行第 `(3)` 步的时候，很有可能会触发 [myListen] 监听函数。
+ *
+ * 这属于「逻辑上」的正常现象，但很有可能不是你所期望的「业务上」的结果。
+ * 目前阶段，你需要自行想办法解决这个问题，例如借助ID或者拦截器等方式在某些情况下阻止某些监听函数的执行。
+ *
+ *
+ */
 @OptIn(ExperimentalSimbotApi::class)
 @Filter("绑定")
 @Listener(priority = PriorityConstant.PRIORITIZED_9)
