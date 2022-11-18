ed058d3a5231be8ef6cc72e7112c4a3e15dea47f
ForteScarlet <ForteScarlet@163.com>
2022-11-18 16:41:30 +0800

    java var

--- simbot-spring-boot-starter-example/spring-boot-webflux-java/src/main/java/simbot/example/task/MessageSendTask.java
+++ simbot-spring-boot-starter-example/spring-boot-webflux-java/src/main/java/simbot/example/task/MessageSendTask.java
@@ -3,8 +3,6 @@ package simbot.example.task;
 import lombok.RequiredArgsConstructor;
 import love.forte.simbot.Identifies;
 import love.forte.simbot.application.Application;
-import love.forte.simbot.bot.BotManager;
-import love.forte.simbot.component.mirai.bot.MiraiBot;
 import love.forte.simbot.component.mirai.bot.MiraiBotManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -34,11 +32,11 @@ public class MessageSendTask {
      */
     @Scheduled(fixedDelay = 1, timeUnit = TimeUnit.HOURS)
     public void task() {
-        for (BotManager<?> botManager : application.getBotManagers()) {
+        for (var botManager : application.getBotManagers()) {
             // 寻找到所需的botManager。例如 MiraiBotManager
             if (botManager instanceof MiraiBotManager miraiBotManager) {
                 // 得到你想要的bot，例如列表中第一个bot
-                final MiraiBot miraiBot = miraiBotManager.all().get(0);
+                final var miraiBot = miraiBotManager.all().get(0);
 
                 // 例如异步地向一个指定好友发送消息
                 miraiBot.getFriendAsync(Identifies.ID(123))
